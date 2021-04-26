b011901ccfb234360b74075262bd763bdad81cc9
kimiko <kimiko675@gmail.com>
2021-04-26 14:20:16 +0200

    Timetable

--- app/src/main/java/com/firstapp/studentplanner/Day.kt
+++ app/src/main/java/com/firstapp/studentplanner/Day.kt
@@ -1,12 +1,8 @@
 package com.firstapp.studentplanner
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
-import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -17,18 +13,42 @@ import kotlinx.android.synthetic.main.activity_day.*
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_list_of_subjects.*
 import kotlinx.android.synthetic.main.dialog_add_subject.*
+import java.util.*
+import java.util.Calendar
 
 
 class Day: AppCompatActivity() {
+
     private lateinit var auth: FirebaseAuth;
-    private var listOfForms = ArrayList<Form>()
+    private var c: Calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        var day2=c.get(Calendar.DAY_OF_WEEK) //dzien w tygodniu np.monday=2
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day)
         val pos:String = intent.getStringExtra("position").toString()
         tvDay.text=pos
-        var dni = arrayListOf<String>("poniedziałek","wtorek","środa","czwartek","piątek","sobota","niedziela")
+        var dni = arrayListOf<String>("poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela")
         var day=dni[1]
+
+        //niedziela jest ustawiona domyslnie jako pierwszy dzien, więc to zamieniam
+        if (day2==1) day2=7
+        else day2-=1
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val dayS =c.get(Calendar.DAY_OF_MONTH)-(day2-1) //data pierwszego dnia w tygodniu
+        val dayy = c.get(Calendar.DAY_OF_MONTH) //obecny dzien
+        val daysInMonth: Int = c.getActualMaximum(Calendar.DAY_OF_MONTH) //ilosc dni w miesiacu
+        var dayE = dayS+6 //data ostatniego dnia w tygodniu
+        var dayS2=dayS
+        var dayE2=dayE
+        if(dayE>daysInMonth) { //jesli jestesmy na przelomie miesiecy
+            dayE = dayE - daysInMonth
+            dayS2=1
+        }
+
+
         auth = FirebaseAuth.getInstance();
         val userId: String = FirebaseAuth.getInstance().currentUser.uid
         val ref = FirebaseDatabase.getInstance().getReference("Users").child(userId).child("Subjects")
@@ -44,9 +64,38 @@ class Day: AppCompatActivity() {
                     val model= i.getValue(Subject::class.java)
                     for (i in model?.forms!!){
                         day = dni[i.dayOfWeek]
-                        if (day==pos) {
-                            val newListObject= ListObject(model, i,i.hour,i.minute)
-                            list2.add(newListObject)
+                        if(day==pos){
+                            if(i.yearStart<year && i.yearEnd>year) {
+                                val newListObject= ListObject(model, i, i.hour, i.minute)
+                                list2.add(newListObject)
+                            }
+                            else if(i.yearStart==year && i.yearEnd==year){
+                                if(i.monthStart<month && i.monthEnd>month) {
+                                    val newListObject= ListObject(model, i, i.hour, i.minute)
+                                    list2.add(newListObject)
+                                }
+                                else if(i.monthStart==month && i.monthEnd==month){
+                                    if (i.dayStart<=dayy &&  i.dayEnd>=dayy){
+
+                                            val newListObject= ListObject(model, i, i.hour, i.minute)
+                                            list2.add(newListObject)
+                                    }
+                                }
+                                /*else if(i.monthStart==month && i.monthEnd>month){
+                                    if (i.dayStart<=dayy &&  i.dayEnd>=dayE){
+
+                                        val newListObject= ListObject(model, i, i.hour, i.minute)
+                                        list2.add(newListObject)
+                                    }
+                                }
+                                else if(i.monthStart<month && i.monthEnd==month){
+                                    if (i.dayStart<=dayS &&  i.dayEnd>=dayy){
+
+                                        val newListObject= ListObject(model, i, i.hour, i.minute)
+                                        list2.add(newListObject)
+                                    }
+                                }*/
+                            }
                         }
                     }
 
